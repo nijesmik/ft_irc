77c1065ad331ddfd68125aed603ac437471bf676
Seokkyu Lee <cd1004s@gmail.com>
2024-08-20 20:47:55 +0900

    feat: RPL_PONG reply to PING command (#4)

--- src/ChatService/ping.cpp
+++ src/ChatService/ping.cpp
@@ -1,8 +1,12 @@
 #include "ChatService.hpp"
 
+#define RPL_PONG(params) ("PONG :" + params + CRLF)
+
 void ChatService::ping(Session &session, const Message &message) {
     std::string token = message.getParam();
     if (token.empty()) {
         return session << NumericReply::get(ERR_NEEDMOREPARAMS, "PING");
-    }
+    } else {
+        session << RPL_PONG(token);
+     }
 }
