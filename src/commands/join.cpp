bba86c285b902c8d3b8f2cf5d861e78deed20a2e
Seokkyu Lee <cd1004s@gmail.com>
2024-08-21 21:46:21 +0900

    feat: JOIN (#9)

--- src/commands/join.cpp
+++ src/commands/join.cpp
@@ -5,7 +5,27 @@
 #include "ChannelService.hpp"
 
 void ChannelService::join(Session &session, const Message &message) {
-    std::string channelName = message.getParam();
-    Channel *channel = getChannel(channelName);
-    channel->join(&session);
+    if (!session.isRegistered()) {
+        return session << NumericReply::get(ERR_NOTREGISTERED);
+    }
+
+    std::vector<std::string> channels = message.getSplitedParam(0, ',');
+    if (channels.empty()) {
+        return session << NumericReply::get(ERR_NEEDMOREPARAMS, "JOIN");
+    }
+
+    std::vector<std::string> keys = message.getSplitedParam(1, ',');
+    std::vector<std::string>::iterator keyIt = keys.begin();
+    for (std::vector<std::string>::iterator channelIt = channels.begin(); channelIt != channels.end(); ++channelIt) {
+        if (!isValidChannel(*channelIt)) {
+            return session << NumericReply::get(ERR_BADCHANMASK, "JOIN");
+        }
+
+        Channel *channel = getChannel(*channelIt);
+        channel->join(&session, keyIt != keys.end() ? *keyIt : std::string());
+
+        if (keyIt != keys.end()) {
+            ++keyIt;
+        }
+    }
 }
