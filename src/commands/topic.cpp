3885cc582278150d64d4d8efd2e5501b39970a60
Seokkyu Lee <cd1004s@gmail.com>
2024-08-22 20:50:36 +0900

    feat: TOPIC validatei (#17)

--- src/commands/topic.cpp
+++ src/commands/topic.cpp
@@ -5,5 +5,16 @@
 #include "ChannelService.hpp"
 
 void ChannelService::topic(Session &session, const Message &message) {
+    if (!session.isRegistered()) {
+        return session << NumericReply::get(ERR_NOTREGISTERED);
+    }
+    if (message.getParam().empty()) {
+        return session << NumericReply::get(ERR_NEEDMOREPARAMS);
+    }
 
+    std::string channelName = message.getParam();
+    std::string topicName = message.getParamsAll(1, ':');
+    Channel *channel = findChannel(channelName);
+
+    //
 }
